f5f9da97092bb8ae250fa1afebbe597ffc0a2c29
Tanzil khan <khantanzil383@gmail.com>
2026-08-07 16:49:54 +0500

    complete authentication controller implementation

---
+++ controllers/authController.js
@@ -0,0 +1,23 @@
+import user from "../models/user";
+import bcrypt from "bcrypt";
+
+export const signupUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existUser = await user.findOne({ email });
+    if (existUser) {
+      return res.status(4001).json({ message: "user already exists" });
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    await user.create({
+        name, 
+        email,
+        password: hashPassword
+    })
+
+    res.status(200).json({message: "user registered successfully"})
+  } catch (error) {
+    res.status(4001).json({ message: "server error", error });
+  }
+};
