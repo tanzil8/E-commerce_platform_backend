10103e480d88b8ed02b4fe12433dcf808233bc41
Tanzil khan <khantanzil383@gmail.com>
2026-08-10 01:34:11 +0500

    implement login API controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,3 +21,40 @@ export const signupUser = async (req, res) => {
     res.status(4001).json({ message: "server error", error });
   }
 };
+
+
+export const loginUser = async (req, res) =>{
+
+  try {
+    const {email, password} = req.body;
+
+   const User = await user.findOne({ email });
+    if (!User) {
+      return res.status(4001).json({ message: "user not exists" });
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      return res.status(400).json({message:"Invalid credentials"})
+    }
+
+    const token = jwt.sign(
+      {id: user._id},
+      process.env.JWT_SECRET,
+      {expiresIn: "7d"}
+    )
+    res.json({
+      message: "Login successful",
+      token,
+      user:{
+        id: user._id,
+        name:user.name,
+        email:user.email
+      }
+    })
+
+  } catch (error) {
+     res.status(4001).json({ message: "server error", error });
+  }
+
+}
