22f1468bf02abc2fc6bf5238a8b2d2f7d9604a67
Tanzil khan <khantanzil383@gmail.com>
2026-08-16 13:24:47 +0500

    integrate Cloudinary configuration

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ import cors from "cors";
 import connectDB from "./config/db.js"
 import router from "./routers/userRouter.js";
 import product from "./routers/productRouter.js"
+import { v2 as cloudinary } from 'cloudinary';
+
+
+ cloudinary.config({ 
+        cloud_name: process.env.CLOUD_NAME, 
+        api_key: process.env.API_KEY, 
+        api_secret: process.env.API_SECRET
+    });
 
 const app = express();
 dotenv.config();
